214831571ab03919a5ad5732278c7c3e3cdd29f2
Komorek <rafa.kaquino@gmail.com>
2025-02-04 00:39:06 -0300

    add base de cores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,11 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        color1: "var(--colorbase-1)",
+        color2: "var(--colorbase-2)",
+        color3: "var(--colorbase-3)",
+        color4: "var(--colorbase-4)",
+        color5: "var(--colorbase-5)",
       },
     },
   },
